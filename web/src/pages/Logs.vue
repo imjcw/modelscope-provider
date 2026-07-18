47431bc8a86183e0c53a577199b912b64d6ac10b
imjcw <imjcw@imjcw.com>
2026-07-19 02:07:10 +0800

    chore: remove clock icon from Logs page header

--- web/src/pages/Logs.vue
+++ web/src/pages/Logs.vue
@@ -1,11 +1,6 @@
 <template>
   <div>
     <PageHeader title="日志">
-      <template #title-prefix>
-        <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" class="text-gray-600">
-          <polyline points="1 4 1 10 7 10"/><path d="M3.51 15a9 9 0 1 0 2.13-9.36L1 10"/>
-        </svg>
-      </template>
       <template #action>
         <button class="btn btn-primary">↻ 刷新</button>
       </template>
